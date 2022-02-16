6a3de1dea5a6272b365b239b00d6987f886f257b
q5089877 <q5089877@gmail.com>
2022-02-16 10:20:11 +0800

    Add project files.

---
+++ FFT_for_DOSE/Form1.Designer.cs
@@ -0,0 +1,177 @@
+﻿namespace FFT_DOSE
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.button2 = new System.Windows.Forms.Button();
+            this.serialPort1 = new System.IO.Ports.SerialPort(this.components);
+            this.button6 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.RS232_TTL = new System.IO.Ports.SerialPort(this.components);
+            this.label1 = new System.Windows.Forms.Label();
+            this.cbx_plc = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.cbx_dose = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.cbx_power = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(12, 41);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(75, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "COM";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // serialPort1
+            // 
+            this.serialPort1.DataReceived += new System.IO.Ports.SerialDataReceivedEventHandler(this.serialPort1_DataReceived);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(12, 12);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 5;
+            this.button6.Text = "Write M2";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button7
+            // 
+            this.button7.Location = new System.Drawing.Point(106, 12);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(75, 23);
+            this.button7.TabIndex = 6;
+            this.button7.Text = "Read M2";
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // RS232_TTL
+            // 
+            this.RS232_TTL.BaudRate = 115200;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Verdana", 12F);
+            this.label1.Location = new System.Drawing.Point(12, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(120, 18);
+            this.label1.TabIndex = 28;
+            this.label1.Text = "PLC ComPort:";
+            // 
+            // cbx_plc
+            // 
+            this.cbx_plc.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cbx_plc.FormattingEnabled = true;
+            this.cbx_plc.Location = new System.Drawing.Point(171, 78);
+            this.cbx_plc.Name = "cbx_plc";
+            this.cbx_plc.Size = new System.Drawing.Size(138, 24);
+            this.cbx_plc.TabIndex = 27;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Verdana", 12F);
+            this.label3.Location = new System.Drawing.Point(12, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(135, 18);
+            this.label3.TabIndex = 32;
+            this.label3.Text = "DOSE ComPort:";
+            // 
+            // cbx_dose
+            // 
+            this.cbx_dose.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cbx_dose.FormattingEnabled = true;
+            this.cbx_dose.Location = new System.Drawing.Point(171, 109);
+            this.cbx_dose.Name = "cbx_dose";
+            this.cbx_dose.Size = new System.Drawing.Size(138, 24);
+            this.cbx_dose.TabIndex = 31;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Verdana", 12F);
+            this.label4.Location = new System.Drawing.Point(12, 142);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(139, 18);
+            this.label4.TabIndex = 34;
+            this.label4.Text = "Power ComPort:";
+            // 
+            // cbx_power
+            // 
+            this.cbx_power.Font = new System.Drawing.Font("PMingLiU", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(136)));
+            this.cbx_power.FormattingEnabled = true;
+            this.cbx_power.Location = new System.Drawing.Point(171, 139);
+            this.cbx_power.Name = "cbx_power";
+            this.cbx_power.Size = new System.Drawing.Size(138, 24);
+            this.cbx_power.TabIndex = 33;
+            this.cbx_power.SelectedIndexChanged += new System.EventHandler(this.cbx_power_SelectedIndexChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(706, 686);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.cbx_power);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.cbx_dose);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.cbx_plc);
+            this.Controls.Add(this.button7);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button2;
+        private System.IO.Ports.SerialPort serialPort1;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button7;
+        private System.IO.Ports.SerialPort RS232_TTL;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.ComboBox cbx_plc;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ComboBox cbx_dose;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox cbx_power;
+    }
+}
+
---
+++ FFT_for_DOSE/Form1.cs
@@ -0,0 +1,365 @@
+﻿using Ivi.Visa;
+using Ivi.Visa.FormattedIO;
+using Ivi.Visa.Interop;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Windows.Forms;
+using System.Management;
+
+namespace FFT_DOSE
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+
+            GetPortInformation();
+        }
+
+
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            serialPort1.PortName = "COM13";
+            serialPort1.BaudRate = 9600;
+            serialPort1.DataBits = 8;
+            serialPort1.Parity = Parity.Even;
+            serialPort1.StopBits = StopBits.One;
+            try
+            {
+                serialPort1.Open();
+                //  timer1.Enabled = true;
+                MessageBox.Show("打开端口");
+            }
+            catch (Exception ea)
+            {
+                MessageBox.Show("打开端口错误");
+            }
+
+        }
+
+        /// <summary>
+        /// 低字节在前
+        /// </summary>
+        /// <param name="pDataBytes"></param>
+        /// <returns></returns>
+        static byte[] CRC16LH(byte[] pDataBytes)
+        {
+            ushort crc = 0xffff;
+            ushort polynom = 0xA001;
+
+            for (int i = 0; i < pDataBytes.Length; i++)
+            {
+                crc ^= pDataBytes[i];
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((crc & 0x01) == 0x01)
+                    {
+                        crc >>= 1;
+                        crc ^= polynom;
+                    }
+                    else
+                    {
+                        crc >>= 1;
+                    }
+                }
+            }
+
+            byte[] result = BitConverter.GetBytes(crc);
+            return result;
+        }
+
+        /// <summary>
+        /// 高字节在前
+        /// </summary>
+        /// <param name="pDataBytes"></param>
+        /// <returns></returns>
+        static byte[] CRC16HL(byte[] pDataBytes)
+        {
+            ushort crc = 0xffff;
+            ushort polynom = 0xA001;
+
+            for (int i = 0; i < pDataBytes.Length; i++)
+            {
+                crc ^= pDataBytes[i];
+                for (int j = 0; j < 8; j++)
+                {
+                    if ((crc & 0x01) == 0x01)
+                    {
+                        crc >>= 1;
+                        crc ^= polynom;
+                    }
+                    else
+                    {
+                        crc >>= 1;
+                    }
+                }
+            }
+
+            byte[] result = BitConverter.GetBytes(crc).Reverse().ToArray();
+            return result;
+        }
+
+
+
+        private void serialPort1_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            byte[] in_data;
+            SerialPort sp = (SerialPort)sender;
+            Thread.Sleep(50);
+
+
+            int bytes = sp.BytesToRead;
+            byte[] buffer = new byte[bytes];
+            sp.Read(buffer, 0, bytes);
+
+            //Console.WriteLine(in_data);
+
+            if (buffer.Length > 3)
+            {
+                if (buffer[1] == 1 && buffer[3] == 2)
+                {
+                    List<byte> list = new List<byte>();
+                    list.Add(0x01);
+                    list.Add(0x05);
+                    list.Add(0x08);
+                    list.Add(0x02);
+                    list.Add(0xFF);
+                    list.Add(0x00);
+                    byte[] array = list.ToArray();
+                    byte[] Crc_data = CRC16LH(array);
+                    list.Add(Crc_data[0]);
+                    list.Add(Crc_data[1]);
+                    byte[] all_array = list.ToArray();
+                    serialPort1.Write(all_array, 0, all_array.Length);
+                }
+            }
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                List<byte> list = new List<byte>();
+                list.Add(0x01);
+                list.Add(0x05);
+                list.Add(0x08);
+                list.Add(0x02);
+                list.Add(0xFF);
+                list.Add(0x00);
+                byte[] array = list.ToArray();
+                byte[] Crc_data = CRC16LH(array);
+                list.Add(Crc_data[0]);
+                list.Add(Crc_data[1]);
+                byte[] all_array = list.ToArray();
+                serialPort1.Write(all_array, 0, all_array.Length);
+            }
+            catch
+            {
+                MessageBox.Show("Com Port Error!!");
+            }
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            List<byte> list = new List<byte>();
+            list.Add(0x01);
+            list.Add(0x01);
+            list.Add(0x08);
+            list.Add(0x02);
+            list.Add(0x00);
+            list.Add(0x01);
+            byte[] array = list.ToArray();
+            byte[] Crc_data = CRC16LH(array);
+            list.Add(Crc_data[0]);
+            list.Add(Crc_data[1]);
+            byte[] all_array = list.ToArray();
+            serialPort1.Write(all_array, 0, all_array.Length);
+        }
+
+        public string GetPortInformation()
+        {
+            cbx_plc.Items.Add("");
+            cbx_dose.Items.Add("");
+            cbx_power.Items.Add("");
+
+            ManagementClass processClass = new ManagementClass("Win32_PnPEntity");
+            ManagementObjectCollection Ports = processClass.GetInstances();
+            foreach (ManagementObject property in Ports)
+            {
+                var name = property.GetPropertyValue("Name");
+                if (name != null && name.ToString().Contains("USB") && name.ToString().Contains("COM"))
+                {
+                    var portInfo = new SerialPortInfo(property);
+                    //Thats all information i got from port.
+                    //Do whatever you want with this information   
+                    cbx_plc.Items.Add(name);
+                    cbx_dose.Items.Add(name);
+                    cbx_power.Items.Add(name);
+                }
+            }
+            return string.Empty;
+        }
+
+        #region Hide Code by PLC
+        //private void button1_Click(object sender, EventArgs e)
+        //{
+        //    List<byte> list = new List<byte>();
+        //    list.Add(0x01);
+        //    list.Add(0x0F);
+        //    list.Add(0x05);
+        //    list.Add(0x00);
+        //    list.Add(0x00);
+        //    list.Add(0x04);
+        //    list.Add(0x01);
+        //    list.Add(0x07);
+        //    //byte[] list = new byte[] { 0x01, 0x0F, 0x05, 0x00, 0x00, 0x04, 0x01, 0x07};
+        //    byte[] array = list.ToArray();
+        //    byte[] Crc_data = CRC16LH(array);
+        //    //   byte[] Set_data = new byte[] { 0x01, 0x0F, 0x05, 0x00, 0x00, 0x04, 0x01, 0x07 };
+
+        //    list.Add(Crc_data[0]);
+        //    list.Add(Crc_data[1]);
+        //    byte[] all_array = list.ToArray();
+        //    serialPort1.Write(all_array, 0, all_array.Length);
+        //}
+
+        //private void button3_Click(object sender, EventArgs e)
+        //{
+        //    List<byte> list = new List<byte>();
+        //    list.Add(0x01);
+        //    list.Add(0x0F);
+        //    list.Add(0x05);
+        //    list.Add(0x00);
+        //    list.Add(0x00);
+        //    list.Add(0x04);
+        //    list.Add(0x01);
+        //    list.Add(0x00);
+        //    //byte[] list = new byte[] { 0x01, 0x0F, 0x05, 0x00, 0x00, 0x04, 0x01, 0x07};
+        //    byte[] array = list.ToArray();
+        //    byte[] Crc_data = CRC16LH(array);
+        //    //   byte[] Set_data = new byte[] { 0x01, 0x0F, 0x05, 0x00, 0x00, 0x04, 0x01, 0x07 };
+
+        //    list.Add(Crc_data[0]);
+        //    list.Add(Crc_data[1]);
+        //    byte[] all_array = list.ToArray();
+        //    serialPort1.Write(all_array, 0, all_array.Length);
+        //}
+
+        //private void button4_Click(object sender, EventArgs e)
+        //{
+        //    List<byte> list = new List<byte>();
+        //    list.Add(0x01);
+        //    list.Add(0x05);
+        //    list.Add(0x05);
+        //    list.Add(0x00);
+        //    list.Add(0xFF);
+        //    list.Add(0x00);
+        //    byte[] array = list.ToArray();
+        //    byte[] Crc_data = CRC16LH(array);
+        //    list.Add(Crc_data[0]);
+        //    list.Add(Crc_data[1]);
+        //    byte[] all_array = list.ToArray();
+        //    serialPort1.Write(all_array, 0, all_array.Length);
+        //}
+
+        //private void button5_Click(object sender, EventArgs e)
+        //{
+        //    List<byte> list = new List<byte>();
+        //    list.Add(0x01);
+        //    list.Add(0x05);
+        //    list.Add(0x05);
+        //    list.Add(0x00);
+        //    list.Add(0x00);
+        //    list.Add(0x00);
+        //    byte[] array = list.ToArray();
+        //    byte[] Crc_data = CRC16LH(array);
+        //    list.Add(Crc_data[0]);
+        //    list.Add(Crc_data[1]);
+        //    byte[] all_array = list.ToArray();
+        //    serialPort1.Write(all_array, 0, all_array.Length);
+        //}
+        #endregion
+
+        private void cbx_power_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            try
+            {
+                string com_name = cbx_power.SelectedItem.ToString();
+                int int_start = com_name.IndexOf("(");
+                int int_end = com_name.IndexOf(")");
+                string com_num = com_name.Substring(int_start + 4, int_end - int_start - 4);
+                // Set address
+                string VISA_ADDRESS = "ASRL" + com_num + "::INSTR";
+
+                // Create a connection (session) to the RS-232 device.                                 
+                IMessageBasedSession session = GlobalResourceManager.Open(VISA_ADDRESS) as IMessageBasedSession;
+
+                // Enable the Termination Character.                
+                session.TerminationCharacterEnabled = true;
+
+
+                // Connection parameters
+                ISerialSession serial = session as ISerialSession;
+                serial.BaudRate = 115200;
+                serial.DataBits = 8;
+                serial.Parity = Ivi.Visa.SerialParity.None;
+                serial.FlowControl = SerialFlowControlModes.DtrDsr;
+
+                // Send the *IDN? and read the response as strings
+                MessageBasedFormattedIO formattedIO = new MessageBasedFormattedIO(session);
+                //   formattedIO.WriteLine("MEASure:CURRent?");
+                formattedIO.WriteLine("*IDN?");
+                string idnResponse = formattedIO.ReadLine();
+
+                Console.WriteLine("Current returned: {0}", idnResponse);
+
+                Thread.Sleep(1000);               
+
+
+                formattedIO.WriteLine("OUTPut 0");
+                formattedIO.WriteLine("OUTPut 0");
+                //     idnResponse = formattedIO.ReadLine();
+
+                Console.WriteLine("Current returned: {0}", idnResponse);
+
+                Thread.Sleep(1000);
+
+                formattedIO.WriteLine("OUTPut 1");
+                formattedIO.WriteLine("OUTPut 1");
+                //   idnResponse = formattedIO.ReadLine();
+
+                Console.WriteLine("Current returned: {0}", idnResponse);
+
+                Thread.Sleep(1000);
+
+                formattedIO.WriteLine("MEASure:voltage?");
+                idnResponse = formattedIO.ReadLine();
+
+                Console.WriteLine("Current returned: {0}", idnResponse);
+
+                // Close the connection to the instrument
+                session.Dispose();
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+                MessageBox.Show("COM port selection error!!");
+            }
+        }
+    }
+}
+
