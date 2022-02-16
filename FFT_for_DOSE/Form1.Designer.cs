697e4ac36dfbdfe8b31828c016f8cb8624d65721
q5089877 <q5089877@gmail.com>
2022-02-16 10:21:00 +0800

    可以開關POWER

--- FFT_for_DOSE/Form1.Designer.cs
+++ FFT_for_DOSE/Form1.Designer.cs
@@ -153,7 +153,7 @@
             this.Controls.Add(this.button6);
             this.Controls.Add(this.button2);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Main";
             this.Load += new System.EventHandler(this.Form1_Load);
             this.ResumeLayout(false);
             this.PerformLayout();
